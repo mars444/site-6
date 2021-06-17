56f9a0aa089eb4a6b1aad04607314ef5bb3ee506
mars444 <petromars444@gmail.com>
2021-06-17 18:38:57 +0300

    added section ourmenu

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,14 @@ $(function(){
     
       });
 
+      $('.ourmenu__slider').slick({
+        nextArrow: '<button type="button" class="slick_button slick-next fa fa-chevron-right"><img src="images/next.svg" alt=""></button>' ,
+        prevArrow: '<button type="button" class="slick_button slick-prev  fa fa-chevron-left"><img src="images/back.svg" alt=""></button>' ,
+        slidesToShow: 4,
+        slidesToScroll: 1
+      
+        });
+  
+
 
 });
-      
