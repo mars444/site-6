a1c7687eb42007531fb521fb70f6ef691abd0727
mars444 <petromars444@gmail.com>
2021-06-17 15:16:07 +0300

    added section hhome

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,10 @@
 $(function(){
+    
+    $('.home__slider').slick({
+      nextArrow: '<button type="button" class="slick_button slick-next fa fa-chevron-right"><img src="images/next.svg" alt=""></button>' ,
+      prevArrow: '<button type="button" class="slick_button slick-prev  fa fa-chevron-left"><img src="images/back.svg" alt=""></button>' ,
+    
+      });
 
 
 });
