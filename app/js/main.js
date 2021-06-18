1329b7f089a0e2bfeced765bf7bcad7644688feb
mars444 <petromars444@gmail.com>
2021-06-18 18:29:26 +0300

    added section contact

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,8 @@ $(function(){
     $('.home__slider').slick({
       nextArrow: '<button type="button" class="slick_button slick-next fa fa-chevron-right"><img src="images/next.svg" alt=""></button>' ,
       prevArrow: '<button type="button" class="slick_button slick-prev  fa fa-chevron-left"><img src="images/back.svg" alt=""></button>' ,
+      autoplay: true,
+      autoplaySpeed: 2000,
     
       });
 
@@ -10,9 +12,12 @@ $(function(){
         nextArrow: '<button type="button" class="slick_button slick-next fa fa-chevron-right"><img src="images/next.svg" alt=""></button>' ,
         prevArrow: '<button type="button" class="slick_button slick-prev  fa fa-chevron-left"><img src="images/back.svg" alt=""></button>' ,
         slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
       
         });
+        $( "#datepicker" ).datepicker();
   
 
 
